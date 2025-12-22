78ccc7b2e307783bee9f3a024be80caa8eb1aff1
Bhumi Ashtaputre <bhumiashtaputre@Milinds-Air.lan>
2025-12-22 17:57:07 +0530

    Check if array is sorted

---
+++ CheckSortedArray.java
@@ -0,0 +1,24 @@
+public class CheckSortedArray {
+
+    public static boolean isSorted(int arr[]){
+
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String args[]){
+
+        int arr[] = {1, 2, 4, 5, 7, 8, 9};
+
+        if(isSorted(arr)){
+            System.out.println("Array is sorted");
+        }
+        else{
+            System.out.println("Array is not sorted");
+        }
+    }
+}
